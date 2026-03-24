e89ee6213630f4f81a92fc0028ef844cd1c18399
siddinenisrinandan-cloud <siddinenisrinandan@gmail.com>
2026-03-25 02:03:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from fastapi import FastAPI
 from api.routes import router
 
-app = FastAPI(
-    title="Chem Agent Lab API",
-    description="Multi-Agent Chemistry Swarm System",
-    version="1.0"
-)
+app = FastAPI()
 
 app.include_router(router)
